2c1fad0f29bd4b30a6f071f33b4c7186a3c1d812
hannahcclark <hannah_clark@mac.com>
2015-03-19 12:22:04 -0400

    fixed typo

--- setup.py
+++
@@ -1,7 +0,0 @@
-setup(
-    name='mymodule',
-    version='0.1.0',
-    install_requires=[],  # Don't put anything here, just use requirements.txt
-    packages=['mymodule'],
-    package_dir={'mymodule': 'src/mymodule'},
-)
