acae5daec5b6fbe821fdc077163f897d5f465c9a
lgodenzi <atomicupcke@gmail.com>
2015-04-24 14:50:35 -0400

    adding login.js

--- login.js
+++ login.js
@@ -1,9 +1,12 @@
 $("document").ready(function () {
-		$("#login").keypress(function(event){
+		$("#login").submit(function(event){
 			var user = $("login-email").val();
 			var pass = $("login-password").val();
 			//entrypts user/password
-			$.post()
+
+			$.post("url", function(data){
+				
+			});
 		});
 
 });
