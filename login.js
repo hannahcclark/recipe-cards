6a79f6fbd799e7ad0ea8c807bce912d4b8dc30db
lgodenzi <atomicupcke@gmail.com>
2015-04-24 13:17:48 -0400

    starting JS

---
+++ login.js
@@ -0,0 +1,9 @@
+$("document").ready(function () {
+		$("#login").keypress(function(event){
+			var user = $("login-email").val();
+			var pass = $("login-password").val();
+			//entrypts user/password
+			$.post()
+		});
+
+});
