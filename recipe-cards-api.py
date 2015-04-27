93a64b675f386faba0c833135832a118e82c6a08
hannahcclark <hannah_clark@mac.com>
2015-04-27 11:37:36 -0400

    beginning flask sessions restructuring server, may or may not be working, hard to test without more pages

--- recipe-cards-api.py
+++ recipe-cards-api.py
@@ -1,9 +1,4 @@
-from flask import Flask
-from flask import abort
-from flask import make_response
-from flask import jsonify
-from flask import request
-from flask import render_template
+from flask import Flask, abort, make_response, jsonify, request, render_template, session, url_for, redirect
 from pymongo import MongoClient
 import bcrypt
 import json
@@ -13,7 +8,9 @@ import logging
 #http://stackoverflow.com/questions/19877903/using-mongo-with-flask-and-python used to resolve issue with objectids and json
 
 app = Flask(__name__)
-
+app.config.update(
+	SECRET_KEY = open("/dev/random","rb").read(32) 
+)
 connection = MongoClient("ds031701.mongolab.com", 31701)
 db = connection["recipe-cards-db"]
 db.authenticate("admin", "password")
@@ -31,24 +28,58 @@ def bad_params(error):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.errorhandler(409)
-def dup_login(error):
-    return make_response(jsonify({'error': 'A user with that username already exists.'}), 409)
-
-@app.errorhandler(422)
-def bad_credentials(error):
-    return make_response(jsonify({'error': 'Login credentials are incorrect.'}), 422)
-
 @app.route('/')
 def index():
-	return render_template('login.html') #Always logs in, change based on session
+	if 'user' in session:
+		recipes = get_recipes_by_user(session['user'])
+		return render_template('index.html', recipes = recipes) #Index needs fixing up
+	return redirect(url_for('login'))
 
-@app.route('/recipes/<recipe_id>', methods=['GET'])
+@app.route('/login/', methods=['GET','POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html')
+	elif request.method == 'POST':
+		data = request.form
+		if  not data or not 'username' in data or not 'password' in data:
+			return render_template('login.html', error="A required field is empty")
+		users = db.users
+		user = users.find_one({"username":data['username']})
+		if user == None or user['password'] != bcrypt.hashpw(data['password'].encode('UTF-8'), user['password'].encode('UTF-8')):
+			return render_template('login.html', error="Invalid username or password")
+		session['user'] = str(user['_id'])
+		return redirect(url_for('index'))
+
+@app.route('/signup/', methods=['GET', 'POST'])
+def create_account():
+	if request.method == 'GET':
+		return render_template('login.html') #swap to signup
+	elif request.method == 'POST':
+		data = request.form
+		if  not data or not 'username' in data or not 'password' in data:
+			return render_template('login.html', error="A required field is empty")#swap to signup
+		users = db.users
+		is_user = users.find_one({"username":data['username']})
+		if is_user == None:
+			password = bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt())
+			users.insert({"username":data['username'], "password":password, "recipes":[]})
+			session['user'] = str(user['_id'])
+			return redirect(url_for('index'))
+		return render_template('login.html', error="Username is already taken")#swap to signup.html
+
+@app.route('/signout/')
+def signout():
+	if 'email' not in session:
+		return redirect(url_for('login'))
+	session.pop('email', None)
+	return redirect(url_for('login'))
+
+@app.route('/recipes/<recipe_id>/', methods=['GET'])
 def recipepage(recipe_id):
 	recipe = json.loads(get_recipe_by_id(recipe_id))['recipe']
 	return render_template('recipe.html', recipe=recipe)
 
-@app.route('/recipe-cards/api/v1.0/recipes/<recipe_id>', methods=['GET'])
+@app.route('/recipe-cards/api/v1.0/recipes/<recipe_id>/', methods=['GET'])
 def get_recipe_by_id(recipe_id):
 	recipes = db.recipes
 	try:
@@ -59,7 +90,6 @@ def get_recipe_by_id(recipe_id):
 	 	abort(404)
 	return json.dumps({'recipe':recipe}, default=doc_encoder) #replace with one that does not give back objid
 
-@app.route('/recipe-cards/api/v1.0/recipes-by-user/<user_id>', methods=['GET'])
 def get_recipes_by_user(user_id):
 	users = db.users
 	try:
@@ -73,13 +103,13 @@ def get_recipes_by_user(user_id):
 	recipeList = []
 	for recipe in recipes:
 	 	recipeList.append({"_id":str(recipe["_id"]),"name":recipe["name"]})
-	return json.dumps(recipeList, default=doc_encoder)
+	return recipeList
 
 @app.route('/recipe-cards/api/v1.0/recipes/recipe-by-url/', methods=['POST'])
 def recipe_from_url():
 	data = request.form
-	if not data or not 'url' in data or not 'userid' in data:
-		abort(400)
+	if not data or not 'url' in data or not 'user' in session:
+		return render_template('index.html', error="A url is required")
 	recipes = db.recipes
 	recipe = recipes.find_one({"url":data['url']})
 	if recipe == None:
@@ -89,32 +119,9 @@ def recipe_from_url():
 	users = db.users
 	user = users.find_one({"_id":ObjectId(str(data['userid']))})
 	if user == None:
-		abort(404)
+		return render_template('index.html', error="An error occured with your account. Please try signing out and logging back in.")
 	users.update({'_id':user['_id']},{'$addToSet':{'recipes': recipeid}})
-	return json.dumps({'recipeid':recipeid}, default=doc_encoder)
+	return redirect(url_for('recipes/' + recipeid))
 
-@app.route('/recipe-cards/api/v1.0/create-account/', methods=['POST'])
-def create_account():
-	data = request.form
-	if  not data or not 'username' in data or not 'password' in data:
-		abort(400)
-	users = db.users
-	is_user = users.find_one({"username":data['username']})
-	if is_user == None:
-		password = bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt())
-		return json.dumps({'userid':users.insert({"username":data['username'], "password":password, "recipes":[]})}, default=doc_encoder)
-	abort(409)
-
-@app.route('/recipe-cards/api/v1.0/login/', methods=['POST'])
-def login():
-	data = request.form
-	if  not data or not 'username' in data or not 'password' in data:
-		abort(400)
-	users = db.users
-	user = users.find_one({"username":data['username']})
-	if user == None or user['password'] != bcrypt.hashpw(data['password'].encode('UTF-8'), user['password'].encode('UTF-8')):
-		abort(422)
-	return json.dumps({'userid':user['_id']}, default=doc_encoder)
-
-if __name__ == '__main__':
+if __name__ == '__main__': 
 	app.run()
