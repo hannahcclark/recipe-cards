823951df97f40eaf523e3716a8e239eabe103d6b
hannahcclark <hannah_clark@mac.com>
2015-04-28 15:02:01 -0400

    directions displayed in modal

--- static/recipe.js
+++ static/recipe.js
@@ -1,19 +1,17 @@
- $(document).ready(function(){
-	
+$(document).ready(function(){
 	$(".modal-wide").on("show.bs.modal", function() {
-		var counter = 1;
-	var height = $(window).height() - 200;
-	$(this).find(".modal-body").css("max-height", height);
-	var sc = counter.toString();
-	var stepnum = "step " + "fdsnklfds" +counter;
-	$("div.modal-title").html(stepnum);
-	$(document).keypress(function(e){
-		 counter++;
-
-		 $("div.modal-title").html("strep " + counter);
-	$("div.modal-body").html("<p>hey " + counter +"</p>");
- });
+		var counter = 0;
+		var height = $(window).height() - 200;
+		$(this).find(".modal-body").css("max-height", height);
+		var sc = counter.toString();
+		var stepinc = counter + 1;
+		$("div.modal-title").html("Step " + stepinc);
+		$("div.modal-body").html("<p>" + $("ol li").eq(counter).text() + "</p>");
+		$(document).keypress(function(e){
+			counter = (counter + 1) % $("ol li").length;
+			stepinc = counter + 1;
+			$("div.modal-title").html("Step " + stepinc);
+			$("div.modal-body").html("<p>" + $("ol li").eq(counter).text() + "</p>");
+ 		});
+	});
 });
-
-
- });
