22554c6b3fb7c30834ebaa5742f4bafa9c9301f2
lgodenzi <atomicupcke@gmail.com>
2015-04-28 14:14:54 -0400

    set up keypress trigger sans forward-backward logic

--- static/recipe.js
+++ static/recipe.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
-$(".modal-wide").on("show.bs.modal", function() {
-  var height = $(window).height() - 200;
-  $(this).find(".modal-body").css("max-height", height);
+ $(document).ready(function(){
+	
+	$(".modal-wide").on("show.bs.modal", function() {
+		var counter = 1;
+	var height = $(window).height() - 200;
+	$(this).find(".modal-body").css("max-height", height);
+	var sc = counter.toString();
+	var stepnum = "step " + "fdsnklfds" +counter;
+	$("div.modal-title").html(stepnum);
+	$(document).keypress(function(e){
+		 counter++;
+
+		 $("div.modal-title").html("strep " + counter);
+	$("div.modal-body").html("<p>hey " + counter +"</p>");
+ });
 });
-});
+
+
+ });
