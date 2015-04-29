6f7e8108e406648527e47eb6e6ae74f68f61be56
hannahcclark <hannah_clark@mac.com>
2015-04-29 19:36:35 -0400

    store search works

--- static/recipe.js
+++ static/recipe.js
@@ -10,7 +10,6 @@
 		$("div#ingreds").html($("ul#ingredients").html());
 		$("div#instructs").html("<p>" + $("ol#preparation li").eq(counter).text() + "</p>");
 		$(document).keydown(function(e){
-			console.log(e.which);
 			if (e.which == 75 || e.which==59 || e.which== 22 || e.which==47 
 				|| e.which == 46 || e.which == 74 || e.which == 39 || e.which == 92
 				|| e.which == 39 || e.which == 40 || e.which == 44 || e.which == 222
@@ -59,7 +58,6 @@
 	 				}
 	 			}
 	 			else {
-	 				console.log("lookup");
 	 				$.ajax('https://maps.googleapis.com/maps/api/geocode/json?address=' 
 	 						+ $("#address").val(),//urlencode
 	 				{
@@ -91,20 +89,36 @@
 });
 function stringifyIngredientList() {
 	var ret = "";
-	for (var i = 0; i < $("ol#preparation li").length; i++) {
-		ret = ret + $("ol#preparation li").eq(i).text() + "\n";
+	for (var i = 0; i < $("ul#ingredients li").length; i++) {
+		ret = ret + $("ul#ingredients li").eq(i).text() + "\n";
 	}
 	return ret;
 }
 
 function getAddress(lat, lng, callback) {
+	var service = new google.maps.places.PlacesService(document.getElementById('map'));
 	var request = {
 		location: new google.maps.LatLng(lat, lng),
-		radius: 20000,
 		types: ["grocery_or_supermarket"],
 		rankBy: google.maps.places.RankBy.DISTANCE
 	};
-	callback("hi");
+	service.nearbySearch(request, function(results, status){
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			var id = results[0].place_id;//Need additional step because not text search
+			service.getDetails({placeId:id}, function(place, status){
+				if (status == google.maps.places.PlacesServiceStatus.OK) {
+					$("#map").html(place.html_attributions);
+					callback(place.name + "\n" + place.formatted_address);
+				}
+				else {
+					console.log("show error");
+				}
+			});
+		}
+		else {
+			console.log("show error");
+		}
+	});
 }
 
 function sendSMS(text) {
